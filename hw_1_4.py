22381e5c447245dd147bccdc9e5878f848c69d36
박태우 <myxodn12@naver.com>
2024-01-15 17:15:16 +0900

    오늘실습

---
+++ hw_1_4.py
@@ -0,0 +1,10 @@
+# 사과는 영어로 apple
+# 바나나는 영어로 banana
+# 키위는 영어로 kiwi
+fruitA = '사과'
+fruitB = '바나나'
+fruitK= '키위'
+English = '영어로 '
+print(f'{fruitA}는 {English}apple')
+print(f'{fruitB}는 {English}banana')
+print(f'{fruitK}는 {English}kiwi')
