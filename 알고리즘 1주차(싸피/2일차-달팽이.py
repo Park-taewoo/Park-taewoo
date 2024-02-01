06d3a44e562c6658493e49ac7da71af34720ec5a
박태우 <myxodn12@naver.com>
2024-02-02 08:26:21 +0900

    알

---
+++ 알고리즘 1주차(싸피/2일차-달팽이.py
@@ -0,0 +1,46 @@
+def solve():
+    r,c = 0,0
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+    arr = [[0]*N for _ in range(N)]
+    cnt = 0
+    move_p = 0
+    while 1:
+        if cnt == N**2:
+            break
+        try:
+            if arr[r][c] != 0:
+                r = r - dr[move_p]
+                c = c - dc[move_p]
+                move_p += 1
+                move_p= move_p% 4              
+                r = r + dr[move_p]
+                c = c + dc[move_p]     
+            cnt += 1
+            arr[r][c] = cnt
+            r = r + dr[move_p]
+            c = c + dc[move_p]
+        except IndexError:
+            r = r - dr[move_p]
+            c = c - dc[move_p]
+            move_p +=1
+            r = r + dr[move_p]
+            c = c + dc[move_p]            
+            move_p= move_p% 4
+    
+    return arr
+        
+    
+    
+
+
+
+
+T = int(input())
+for i in range(1, T + 1):
+    N = int(input())
+    result = solve()
+    print(f'#{i}')
+    # 각 행을 문자열로 변환하여 출력
+    for row in result:
+        print(" ".join(map(str, row)))
