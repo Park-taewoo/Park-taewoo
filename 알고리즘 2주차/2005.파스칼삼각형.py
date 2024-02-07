d4b23c68a1aaf83dede62a03012a1416f5a1bfb2
박태우 <myxodn12@naver.com>
2024-02-07 21:08:34 +0900

    "파스칼"

--- 알고리즘 2주차/2005.파스칼삼각형.py
+++ 알고리즘 2주차/2005.파스칼삼각형.py
@@ -1,13 +1,15 @@
-def solve():
-    cnt=0
-    for i in range(N):
-        cnt+=1
-        return cnt
-
-
-
-T=int(input())
-for tc in range(1,T+1):
+T = int(input())
+for tc in range(1, T + 1):
     N = int(input())
-    result =solve()
-    print(f'#{tc} {result}')
+    memo = [[0] * N for _ in range(N)]
+ 
+    for n in range(N):
+        for r in range(0, n + 1):
+            if r == 0 or n == r:
+                memo[n][r] = 1
+            else:
+                memo[n][r] = memo[n-1][r-1] + memo[n-1][r]
+ 
+    print(f'#{tc}')
+    for n in range(N):
+        print(*memo[n][:n+1])
